dc75438c23ffa2989103bad8cf6a4d11e55443db
xMKWYx <138711719+xMKWYx@users.noreply.github.com>
2023-07-07 00:48:58 +0200

    Add files via upload

---
+++ mkwy-antibh/client.lua
@@ -0,0 +1,31 @@
+-- CONFIG: MOŻESZ ZMIENIĆ "NumberJump" OD (10) DO (30) (Zalecane: 15)
+
+local NumberJump = 15
+
+Citizen.CreateThread(function()
+  local Jump = 0
+  while true do
+
+      Citizen.Wait(1)
+
+      local ped = PlayerPedId()
+
+      if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
+
+        Jump = Jump + 1
+
+          if Jump == NumberJump then
+
+              SetPedToRagdoll(ped, 5000, 1400, 2)
+
+              Jump = 0
+
+          end
+
+      else 
+
+          Citizen.Wait(500)
+          
+      end
+  end
+end)
